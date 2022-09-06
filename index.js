3ecff94ec3dbfb26d716b3ab852a198f4672ab6f
henrymon342 <henrymon342@gmail.com>
2022-09-06 17:14:43 -0400

    add cors options

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const cloudinary = require('./utils/cloudinary');
 const  app  =  express()
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+const corsOptions ={
+  origin:'http://localhost:4200', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
