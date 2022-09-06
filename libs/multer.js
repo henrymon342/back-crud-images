e47ee56345ea09afe95b8f0696cebb7fc9720099
henrymon342 <henrymon342@gmail.com>
2022-09-06 18:10:29 -0400

    fix lugar de almacenaje tmp 3

--- libs/multer.js
+++ libs/multer.js
@@ -7,7 +7,7 @@ const multer = require('multer')
 const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.diskStorage({
-    destination: 'uploads',
+    destination: '/tmp/task/uploads',
     filename: ( req, file, cb) => {
         cb( null, uuidv4() + path.extname(file.originalname))
     }
