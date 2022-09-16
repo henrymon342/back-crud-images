f2944d8e58414c00e957e52e473fbe9ab792e22d
henrymon342 <henrymon342@gmail.com>
2022-09-16 17:46:35 -0400

    add dialectmodule object

--- models/index.js
+++ models/index.js
@@ -5,10 +5,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+let config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 
+
+config = Object.assign(config, {dialectModule: require('mysql2')});
+console.log("config", config);
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
