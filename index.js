9396c6e89f4f503f7b1da13fe5af611a6ca00fd5
henrymon342 <henrymon342@gmail.com>
2023-01-31 17:52:35 -0400

    add extend limit of client request

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+// fixing "413 Request Entity Too Large" errors
+app.use(express.json({limit: "10mb", extended: true}))
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 
 
 
