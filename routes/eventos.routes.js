411d1314473492943da25015a3d10831bafebc56
henrymon342 <henrymon342@gmail.com>
2023-06-05 21:35:01 -0400

    add gestion en busqueda 3

--- routes/eventos.routes.js
+++ routes/eventos.routes.js
@@ -171,7 +171,7 @@ router.post('/findByMonth', async (req, res) => {
 
   arr.forEach(evento => {
      const fechaevento = moment(evento.fechaini);
-    if( fechaevento.month() == nromes && gestion == fechaevento.getFullYear()() ){
+    if( fechaevento.month() == nromes && gestion == fechaevento.getFullYear() ){
       eventos_parciales.push(evento);
     }
     // if( evento.tipofecha == 'VARIOS DÍAS' ){
